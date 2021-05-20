cd816034171074dd2d44d12223b78c57b2c089c6
201524547 <qht6@naver.com>
2021-05-20 11:02:15 +0900

    Key managemenmt/ createMaster, createChild

--- src/feature/keyManagement/screen/CreateWalletScreen.tsx
+++ src/feature/keyManagement/screen/CreateWalletScreen.tsx
@@ -1,22 +1,44 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
-import {createKey, generateNewMnemonic} from '../function/createKey';
+import {Button, View} from 'react-native';
+import WalletItem from '../components/WalletItem';
+import {createChildKey} from '../function/createChild';
+import {createMaster, generateNewMnemonic} from '../function/createMaster';
+import {WalletType, KeyType} from '../utils/types';
 
 const CreateWalletScreen = () => {
   const [test, setTest] = useState('');
-  const [privateKey, setPrivateKey] = useState<string | undefined>('');
+  const [index, setIndex] = useState(0);
+  const [wallets, setWallets] = useState<WalletType[]>([]);
+  const [masterWallet, setMasterWallet] = useState<KeyType>(null);
 
   useEffect(() => {
     generateNewMnemonic().then(mnemonic => setTest(mnemonic));
   }, []);
   useEffect(() => {
-    if (test !== '') setPrivateKey(createKey(new Buffer(test)).privateKey);
+    if (test !== '') setMasterWallet(createMaster(new Buffer(test)));
   }, [test]);
+
+  const addWallet = () => {
+    const childKey = createChildKey(
+      masterWallet.privateKey,
+      masterWallet.chainCode,
+      index,
+    );
+    setIndex(index + 1);
+    const wallet = {...childKey, walletName: 'firstWallet'};
+    setWallets([...wallets, wallet]);
+  };
 
   return (
     <View>
-      <Text>{test}</Text>
-      <Text>{privateKey}</Text>
+      {wallets.map(wallet => (
+        <WalletItem
+          key={wallet.privateKey?.toString()}
+          privateKey={wallet.privateKey.toString('hex')}
+          walletName={wallet.walletName}
+        />
+      ))}
+      <Button title={'add Wallet'} onPress={addWallet} />
     </View>
   );
 };
