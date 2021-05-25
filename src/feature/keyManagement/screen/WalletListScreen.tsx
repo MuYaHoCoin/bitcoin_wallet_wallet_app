9e1839ad175879ff0fe6dc8bb7800c03ab6a69cc
201524547 <qht6@naver.com>
2021-05-25 16:30:48 +0900

    conflicts resolution

--- src/feature/keyManagement/screen/WalletListScreen.tsx
+++ src/feature/keyManagement/screen/WalletListScreen.tsx
@@ -1,22 +1,4 @@
 import React, { useState } from 'react'
-<<<<<<< HEAD
-import { View } from 'react-native'
-import WalletItem from '../components/WalletItem';
-
-type Wallet = {
-    privateKey: string;
-    walletName: string;
-} 
-
-const WalletListScreen = () => {
-    const [wallets, setWallets] = useState<Wallet[]>([
-        {privateKey: "123", walletName:"first"},{privateKey: "456", walletName:"second"}
-    ]);
-    
-    return (
-        <View>
-           {wallets.map(wallet => <WalletItem key={wallet.privateKey} privateKey={wallet.privateKey} walletName={wallet.walletName}/>)} 
-=======
 import { Button, View } from 'react-native'
 import WalletItem from '../components/WalletItem'
 
@@ -35,7 +17,6 @@ const WalletListScreen = () => {
         <View>
             {wallets.map((wallet)=><WalletItem key={wallet.privateKey} privateKey={wallet.privateKey} walletName={wallet.walletName}/>)}
             <Button title={"Add Wallet"} onPress={addWallet}/>
->>>>>>> 2118a48afcd61013aa1debb09bf59ee31aa5cbd6
         </View>
     )
 }
