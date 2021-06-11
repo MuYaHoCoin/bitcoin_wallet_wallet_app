78bb1207e7fee1524cd0964d2fe059b6e61e053e
hoho00 <jhoya963@gmail.com>
2021-06-11 13:07:26 +0900

    make getTransactionList

--- src/feature/transaction/function/transactionFunction.js
+++ src/feature/transaction/function/transactionFunction.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-var rootUrl = 'https://api.blockcypher.com/v1/btc/';
 export const getBalance = async (address, network = 'bitcoinTestNet') => {
+  var rootUrl = 'https://api.blockcypher.com/v1/btc/';
   switch (network) {
     case 'bitcoin': {
       rootUrl += 'main/';
@@ -13,8 +13,39 @@ export const getBalance = async (address, network = 'bitcoinTestNet') => {
     }
     default:
   }
-  await axios.get(rootUrl + '/addrs/' + address).then(d => {
-    console.log(d.data);
-    return d.data.balance;
-  });
+  const balance = await axios.get(rootUrl + '/addrs/' + address);
+  return balance.data.balance;
 };
+
+export const getBTCCurrentPrice = () => {
+  const price = await axios.get(
+    'https://api.coindesk.com/v1/bpi/currentprice.json',
+  );
+  console.log(price.data.bpi.USD.rate);
+  return price.data.bpi.USD.rate;
+};
+
+export const getTransactionList = async (
+  address,
+  network = 'bitcoinTestNet',
+) => {
+  var rootUrl = 'https://api.blockcypher.com/v1/btc/';
+  switch (network) {
+    case 'bitcoin': {
+      rootUrl += 'main/';
+      break;
+    }
+    case 'bitcoinTestNet': {
+      rootUrl += 'test3/';
+      break;
+    }
+    default:
+  }
+  const txs = await (
+    await axios.get(rootUrl + '/addrs/' + address)
+  ).data.txrefs.slice(0, 10);
+  console.log(txs);
+  return txs;
+};
+
+export const creatTransaction = () => {};
